d7e97a2e4681033996919509d9bfbd931cc4711a
leo_zhao <leozhao@server.leozhao.ca>
2016-01-27 23:41:33 +0300

    server settings

--- core/options.py
+++ core/options.py
@@ -3,5 +3,5 @@ from tornado.options import define
 def define_db():
     define('mongo_db', type=str, default='lzvault',
         help='The MongoDB to connect to.')
-    define('mongo_host', type=str, default='localhost',
+    define('mongo_host', type=str, default='127.0.0.1',
         help='Location of the MongoDB to connect to.')
